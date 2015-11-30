07c8eeea85157de6acd83c803eb6e81e7a9e0352
jodge <georgeorti@gmail.com>
2015-12-01 00:05:44 +0300

    updated config files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
 
-var morgan = require('morgan');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -23,107 +23,132 @@ mongoose.connect(configDB.url); // connect to our databse
 require('./config/passport')(passport); // pass passport to configuration
 
 // configure our express middleware
-
-
-
-var express = require('express'),
-	routes = require('./routes'),
-	http = require('http'),
-	path = require('path'),
-	mongoose = require('mongoose'),
-	models = require('./models'),
-	dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/nodejs',
-	db = mongoose.connect(dbUrl, {safe : true});
-
-// Express.js Middleware
-var logger = require('morgan'),
-	errorHandler = require('errorhandler'),
-	bodyParser = require('body-parser'),
-	methodOverride = require('method-override'),
-	cookieParser = require('cookie-parser'),
-	session = require('express-session');
-
-var app = express();
-app.locals.appTitle = 'NodeJS Blog';
-
-app.use(function(req, res, next) {
-	if(!models.Post && !models.User)
-		return next(new Error("No models"))
-	req.models = models;
-	return next();
-});
-
-// Express.js configuration
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// Express.js middleware configuration
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(session({secret : '3T67774A-R649-4D44-9735-43E296ZZ980F', resave : true, saveUninitialized : true}));
+app.use(logger('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 
-// Authentication middleware
-app.use(function(req, res, next) {
-	if (req.session && req.session.logged)
-		res.locals.logged = true;
-	next();
-});
+// setup for templating
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// Authorization middleware
-var authorize = function(req, res, next) {
-	if (req.session && req.session.logged) 
-		return next();
-	else
-		return res.redirect('/login');
-};
+// required for passport
+app.use(session({secret : '3T67774A-R649-4D44-9735-43E296ZZ980F', resave : true, saveUninitialized : true}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-// development only
-if ('development' == app.get('env')) {
-	app.use(errorHandler());
-}
+// routes =======================================================================
+require('./routes')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// pages and routes
-app.get('/', routes.index);
-app.get('/posts/:slug', routes.article.show);
-app.get('/post', authorize, routes.article.post);
-app.post('/post', authorize, routes.article.postArticle);
-app.get('/manage', authorize, routes.article.manage)
-app.get('/login', routes.user.login);
-app.post('/login', routes.user.authenticate);
-app.get('/register', routes.user.register);
-app.post('/register', routes.user.registerUser);
-app.get('/logout', routes.user.logout);
+// launch ======================================================================
+app.listen(port);
+console.log('Express server listening to port ' + port);
 
-// REST API routes
-app.all('/api', authorize);
-app.get('/api/articles', routes.article.list);
-app.post('/api/articles', routes.article.add);
-app.put('/api/articles/:id', routes.article.edit);
-app.delete('/api/articles/:id', routes.article.delete);
 
-app.all('*', function(req, res) {
-	res.sendStatus(404);
-});
 
-var server = http.createServer(app);
-var boot = function() {
-	server.listen(app.get('port'), function() {
-		console.log('Express server listening to port ' + app.get('port'));
-	});
-}
-var shutdown = function() {
-	server.close();
-}
-if (require.main === module) {
-	boot();
-} else {
-	console.info('Running app as a module')
-	exports.boot = boot;
-	exports.shutdown = shutdown;
-	exports.port = app.get('port');
-}
+
+
+// var express = require('express'),
+// 	routes = require('./routes'),
+// 	http = require('http'),
+// 	path = require('path'),
+// 	mongoose = require('mongoose'),
+// 	models = require('./models'),
+// 	dbUrl = process.env.MONGOHQ_URL || 'mongodb://@localhost:27017/nodejs',
+// 	db = mongoose.connect(dbUrl, {safe : true});
+
+// // Express.js Middleware
+// var logger = require('morgan'),
+// 	errorHandler = require('errorhandler'),
+// 	bodyParser = require('body-parser'),
+// 	methodOverride = require('method-override'),
+// 	cookieParser = require('cookie-parser'),
+// 	session = require('express-session');
+
+// var app = express();
+// app.locals.appTitle = 'NodeJS Blog';
+
+// app.use(function(req, res, next) {
+// 	if(!models.Post && !models.User)
+// 		return next(new Error("No models"))
+// 	req.models = models;
+// 	return next();
+// });
+
+// // Express.js configuration
+// app.set('port', process.env.PORT || 3000);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// // Express.js middleware configuration
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieParser());
+// app.use(session({secret : '3T67774A-R649-4D44-9735-43E296ZZ980F', resave : true, saveUninitialized : true}));
+// app.use(methodOverride());
+// app.use(express.static(__dirname + '/public'));
+
+// // Authentication middleware
+// app.use(function(req, res, next) {
+// 	if (req.session && req.session.logged)
+// 		res.locals.logged = true;
+// 	next();
+// });
+
+// // Authorization middleware
+// var authorize = function(req, res, next) {
+// 	if (req.session && req.session.logged) 
+// 		return next();
+// 	else
+// 		return res.redirect('/login');
+// };
+
+// // development only
+// if ('development' == app.get('env')) {
+// 	app.use(errorHandler());
+// }
+
+// // pages and routes
+// app.get('/', routes.index);
+// app.get('/posts/:slug', routes.article.show);
+// app.get('/post', authorize, routes.article.post);
+// app.post('/post', authorize, routes.article.postArticle);
+// app.get('/manage', authorize, routes.article.manage)
+// app.get('/login', routes.user.login);
+// app.post('/login', routes.user.authenticate);
+// app.get('/register', routes.user.register);
+// app.post('/register', routes.user.registerUser);
+// app.get('/logout', routes.user.logout);
+
+// // REST API routes
+// app.all('/api', authorize);
+// app.get('/api/articles', routes.article.list);
+// app.post('/api/articles', routes.article.add);
+// app.put('/api/articles/:id', routes.article.edit);
+// app.delete('/api/articles/:id', routes.article.delete);
+
+// app.all('*', function(req, res) {
+// 	res.sendStatus(404);
+// });
+
+// var server = http.createServer(app);
+// var boot = function() {
+// 	server.listen(app.get('port'), function() {
+// 		console.log('Express server listening to port ' + app.get('port'));
+// 	});
+// }
+// var shutdown = function() {
+// 	server.close();
+// }
+// if (require.main === module) {
+// 	boot();
+// } else {
+// 	console.info('Running app as a module')
+// 	exports.boot = boot;
+// 	exports.shutdown = shutdown;
+// 	exports.port = app.get('port');
+// }
--- config/passport.js
+++ config/passport.js
@@ -34,7 +34,7 @@ module.exports = function(passport) {
 		// by default, local strategy uses username and password, we will override with email
 		usernameField : 'email',
 		passwordField : 'password',
-		passReqToCallback : true // allows us to pass in the req from our router (lets us check that a use ris logged in or not)
+		passReqToCallback : true // allows us to pass in the req from our router (lets us check that a use ris logged in or not
 	}, function(req, email, password, done) {
 		if (email)
 			email.toLowerCase(); // use lower-case emails to avoid case-sensitive email matching
@@ -63,4 +63,70 @@ module.exports = function(passport) {
 	// ========================================================================
 	// LOCAL SIGNUP ===========================================================
 	// ========================================================================
+	passport.use('local-signup', new LocalStrategy({
+		// by default, local strategy uses username and password, we will override with email
+		usernameField : 'email',
+		passwordField : 'password',
+		passReqToCallback : true // allows us to pass in the req from our router (lets us check that a use ris logged in or not
+	}, function(req, email, password, done) {
+		if (email) 
+			email  = email.toLowerCase(); // use lower-case emails to avoid case-sensitive email matching
+
+		// asynchronous
+		process.nextTick(function() {
+			if (!req.user) {
+
+				User.findOne({'local.email' : email}, function(err, user) {
+					// if there is any error, return the error
+					if (err)
+						return done(err);
+
+					// check to see if theres already a user with that email
+					if (user) {
+						return done(null, false, req.flash('signupMessage', 'That email is alraedy taken'));
+					} else {
+						// create the user
+						var newUser = new User();
+
+						newUser.local.email = email;
+						newUser.local.password = newUser.generateHash(password);
+
+						newUser.save(function(err) {
+							if (err)
+								return done(err);
+
+							return done(null, newUser);
+						});
+					}
+
+				}
+			} else if(!req.user.local.email) {
+				// ...presumably they're trying to connect a local account
+                // BUT let's check if the email used to connect a local account is being used by another user
+                User.findOne({ 'local.email' :  email }, function(err, user) {
+                    if (err)
+                        return done(err);
+                    
+                    if (user) {
+                        return done(null, false, req.flash('loginMessage', 'That email is already taken.'));
+                        // Using 'loginMessage instead of signupMessage because it's used by /connect/local'
+                    } else {
+                        var user = req.user;
+                        user.local.email = email;
+                        user.local.password = user.generateHash(password);
+                        user.save(function (err) {
+                            if (err)
+                                return done(err);
+                            
+                            return done(null,user);
+                        });
+                    }
+                });
+
+		} else {
+			// user is logged in and already has a local account. Ignore signup. (You should log out before trying to create a new account, user!)
+            return done(null, req.user);
+            });
+		})
+	}));
 };
